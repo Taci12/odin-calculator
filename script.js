a758c1e2d204567d534bd5886501760ecab2cd5e
Taciano <taciano3003@gmail.com>
2023-02-01 11:32:40 -0300

    trying to add keyboard functionality. all buttons work except the operators.

--- script.js
+++ script.js
@@ -85,3 +85,19 @@ function pressedClearButton() {
     b = '';
     newOperation = true;
 }
+
+document.addEventListener('keydown', (event) => {
+    let operators = ['/', '*', '+', '-'];
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    if (event.key in numbers) {
+        addInDisplay(event.key);
+    } else if (event.key in operators) {
+        assignOperator(event.key);
+    } else if (event.key == 'Backspace') {
+        pressedBackspaceButton();
+    } else if (event.key == 'Enter') {
+        pressedEqualsButton();
+    }
+    console.log(event.key);
+    console.log(typeof (event.key));
+})
